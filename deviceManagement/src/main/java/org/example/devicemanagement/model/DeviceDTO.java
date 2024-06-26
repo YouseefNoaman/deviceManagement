549d89a3239a0a7b95224e58744951f50597f4fd
Youseef Noaman <youseefnoaman@gmail.com>
2024-06-26 01:50:25 +0100

    Update DeviceDTO.java

--- deviceManagement/src/main/java/org/example/devicemanagement/model/DeviceDTO.java
+++ deviceManagement/src/main/java/org/example/devicemanagement/model/DeviceDTO.java
@@ -4,13 +4,23 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
 
 @Getter
 @Setter
-public class DeviceDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeviceDTO implements Serializable {
 
+	@Serial
+	private static final long serialVersionUID = 1398949794185445462L;
+	
 	@NotBlank
 	@Size(max = 255)
 	private String name;
